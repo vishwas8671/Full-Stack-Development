b57354ef3d7b1423c2683545a8d3fbc2e08795a0
Vishwas <159616254+vishwas8671@users.noreply.github.com>
2026-06-20 23:47:45 +0530

    Clean up auth.middleware.js code
    Removed unnecessary whitespace and comments in auth middleware.

--- Chai aur Backend/src/middlewares/auth.middleware.js
+++ Chai aur Backend/src/middlewares/auth.middleware.js
@@ -11,9 +11,7 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
         }
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
-    
         if (!user) {
-            
             throw new ApiError(401, "Invalid Access Token")
         }
     
